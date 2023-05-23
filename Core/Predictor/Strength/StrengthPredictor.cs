4a66e081900555ef663e357a02de6eeb03152328
Vincent REMY <vincentremy222@gmail.com>
2023-05-23 23:12:17 +0200

    feat : add StrengthPredictor type

---
+++ Core/Predictor/Strength/StrengthPredictor.cs
@@ -0,0 +1,14 @@
+﻿using App.Enumerator;
+using App.Interface;
+
+namespace App.Core.Predictor.Strength
+{
+    abstract public class StrengthPredictor : AbstractPredictor
+    {
+        public StrengthPredictor(IIndicator indicator, DecisionMaker decisionMaker) : base(indicator, decisionMaker)
+        {
+        }
+
+        abstract public EMarketForce GetStrength();
+    }
+}
