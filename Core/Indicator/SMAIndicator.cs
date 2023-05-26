16a85f111bce6289ce3917c01b49621a477dedd2
Vincent REMY <vincentremy222@gmail.com>
2023-05-26 22:43:48 +0200

    feat : add SMAIndicator

---
+++ Core/Indicator/SMAIndicator.cs
@@ -0,0 +1,24 @@
+﻿using App.Entity;
+using App.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Indicator
+{
+    /**
+     * Indicateur de moyenne mobile
+     */
+    public class SMAIndicator : IIndicator<decimal>
+    {
+        public decimal Calculate(params object[] objects)
+        {
+            List<Candle> candles = (List<Candle>)objects[0];
+            int period = candles.Count;
+
+            return candles.Sum(c => c.Close) / period;
+        }
+    }
+}
